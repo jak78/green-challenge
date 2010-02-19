01921cbd5f8b95dda580a92e3f7a7d4b8d35e549
henri.tremblay <henri.tremblay@75506b02-0ce9-11df-aac2-bbd07866f500>
2010-02-19 14:11:08 +0000

    Initial version

---
+++ Image2Rle/src/main/java/com/octo/greenchallenge/Image2rle.java
@@ -0,0 +1,53 @@
+package com.octo.greenchallenge;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.imageio.ImageIO;
+
+public class Image2rle {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			showUsage("Missing image file name");
+			return;
+		}
+
+		String filename = args[0];
+		File file = new File(filename);
+		if (!file.exists()) {
+			showUsage("File " + filename + " not found");
+			return;
+		}
+
+		try {
+			BufferedImage image = ImageIO.read(file);
+
+			int[] result = RleEncoder.encode(image);
+
+			String rleFile = filename.substring(0, filename.lastIndexOf('.'));
+			DataOutputStream out = new DataOutputStream(
+					new BufferedOutputStream(new FileOutputStream(rleFile + ".qrc")));
+			for (int i : result) {
+				out.writeInt(i);
+			}
+
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			showUsage(e.toString());
+			return;
+		}
+	}
+
+	private static void showUsage(String msg) {
+		System.err.println("Error: " + msg);
+		System.err.println();
+		System.err.println("Usage: " + Image2rle.class.getSimpleName() + " filename");
+		System.err.println();
+		System.exit(1);
+	}
+}
