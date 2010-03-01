c46231b3b0bba217328822d9ff19cb5fac9085c0
rudy.krol <rudy.krol@75506b02-0ce9-11df-aac2-bbd07866f500>
2010-03-01 22:48:28 +0000

    ajout de logs sur les temps CPU calculés

--- QRDecoder/src/main/java/com/octo/greenchallenge/qrdecoder/QRDecoderServlet.java
+++ QRDecoder/src/main/java/com/octo/greenchallenge/qrdecoder/QRDecoderServlet.java
@@ -1,6 +1,7 @@
 package com.octo.greenchallenge.qrdecoder;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,19 +10,35 @@ import javax.servlet.http.HttpServletResponse;
 import jp.sourceforge.qrcode.QRCodeDecoder;
 import jp.sourceforge.qrcode.util.ContentConverter;
 
+import com.google.appengine.api.quota.QuotaService;
+import com.google.appengine.api.quota.QuotaServiceFactory;
+
 @SuppressWarnings("serial")
 public class QRDecoderServlet extends HttpServlet {
+
+	private static final Logger log = Logger.getLogger(QRDecoderServlet.class
+			.getName());
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		
-		// long start = System.currentTimeMillis();
 
-		QRCodeDecoder decoder = new QRCodeDecoder();
-		String decodedString = processDecode("WEB-INF/qrc/apple.qrc", decoder);
+		String decodedString = "";
+		QuotaService qs = QuotaServiceFactory.getQuotaService();
+		for (int j = 0; j < 10; j++) {
+			long start = qs.getCpuTimeInMegaCycles();
+			for (int i = 0; i < 100; i++) {
 
-		// long processTime = System.currentTimeMillis() - start;
-		
+				QRCodeDecoder decoder = new QRCodeDecoder();
+				decodedString = processDecode("WEB-INF/qrc/apple.qrc", decoder);
+
+			}
+			long end = qs.getCpuTimeInMegaCycles();
+			double cpuSeconds = qs.convertMegacyclesToCpuSeconds(end - start);
+			log.info("CPU en secondes : " + cpuSeconds
+					+ " / CPU en Megacycles : " + (end - start));
+		}
+
 		resp.getWriter().println(decodedString);
 	}
 
