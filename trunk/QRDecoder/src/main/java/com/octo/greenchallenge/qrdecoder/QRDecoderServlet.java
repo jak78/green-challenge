b9f1bb68921185ae97bf46bd655644c8c4ac4487
stephen.perin@gmail.com <stephen.perin@gmail.com@75506b02-0ce9-11df-aac2-bbd07866f500>
2010-03-31 13:55:37 +0000

    Ajout d'une première version de POST des résultats vers score-challenge.

--- trunk/QRDecoder/src/main/java/com/octo/greenchallenge/qrdecoder/QRDecoderServlet.java
+++ trunk/QRDecoder/src/main/java/com/octo/greenchallenge/qrdecoder/QRDecoderServlet.java
@@ -1,17 +1,30 @@
-package com.octo.greenchallenge.qrdecoder;
+Ôªøpackage com.octo.greenchallenge.qrdecoder;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Logger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import sun.net.www.http.HttpClient;
 
 import jp.sourceforge.qrcode.QRCodeDecoder;
 import jp.sourceforge.qrcode.util.ContentConverter;
 
 import com.google.appengine.api.quota.QuotaService;
 import com.google.appengine.api.quota.QuotaServiceFactory;
+
+import com.google.appengine.api.urlfetch.HTTPMethod;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
 
 @SuppressWarnings("serial")
 public class QRDecoderServlet extends HttpServlet {
@@ -30,29 +43,46 @@ public class QRDecoderServlet extends HttpServlet {
 
 		String decodedString = "";
 		QuotaService qs = QuotaServiceFactory.getQuotaService();
-		// plusieurs executions afin de voir si les résultats sont reproductibles, (et pour faire 1 moyenne)
+		
+		// plusieurs executions afin de voir si les r√©sultats sont reproductibles, (et pour faire 1 moyenne)
+		
+		long loopStart = qs.getCpuTimeInMegaCycles();
+		
 		for (int j = 0; j < decodeCycle; j++) {
 			long start = qs.getCpuTimeInMegaCycles();
 			//for (int i = 0; i < 100; i++) {
 			log.info("Start : "+start);
 				QRCodeDecoder decoder = new QRCodeDecoder();
-				//affichage des valeurs décodées à la dernière exécution
+				QRCodeDecoder.setCanvas(
+						new jp.sourceforge.qrcode.util.DebugCanvasAdapter(){
+							public  void println(String message){
+								//QRDecoderServlet.log.info("DebugCanvas: " +  message);
+								System.out.println(message);
+							};
+						}
+				);
+				//affichage des valeurs d√©cod√©es √† la derni√®¬ère execution
 				for(int k=0;k<filenames.length ;k++){
 					if (j==(decodeCycle-1)) {
 						decodedString += processDecode(rootPath+filenames[k]+imageType, decoder);
 						decodedString += "\\n";
 					}
 				}
-				
-
 			//}
 			long end = qs.getCpuTimeInMegaCycles();
 			log.info("End : "+end);
 			double cpuSeconds = qs.convertMegacyclesToCpuSeconds(end - start);
 			log.info("CPU en secondes : " + cpuSeconds
-					+ " / CPU en Megacycles : " + (end - start));
+					+ " / CPU en Megacycles : " + (end - start));							
 		}
-
+		
+		long cpuMegacyclesValue = qs.getCpuTimeInMegaCycles() - loopStart;
+		String challengerID = "spr-GAE";
+		// Probl√®me : source "GAE" non support√©e ? 
+		String source = "GREEN_FOX";
+						
+		postResults(challengerID, cpuMegacyclesValue, source);
+				
 		resp.getWriter().println(decodedString);
 	}
 
@@ -64,4 +94,41 @@ public class QRDecoderServlet extends HttpServlet {
 		return decodedString;
 	}
 
+	private void postResults(String challengerID, long cpuMegacyclesValue, String source){
+        try {
+        	String encoding = "UTF-8";
+        	String cpuMegacycles = URLEncoder.encode( Long.toString(cpuMegacyclesValue ), encoding);			
+    		challengerID = URLEncoder.encode(challengerID, encoding);
+    		source = URLEncoder.encode(source, encoding);
+        	
+        	log.info("TRYING POST: challengerID=" + challengerID +" ; CPUCycles=" + cpuMegacycles+" ; source=" + source);
+        	
+            URL url = new URL("http://score-challenge.appspot.com/api/collect/cpu");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write("challengerID=" + challengerID);
+            writer.write("&CPUCycles=" + cpuMegacycles);
+            writer.write("&source=" + source);
+            writer.close();
+            
+            int responseCode = connection.getResponseCode();
+            
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+            	// OK!
+            	log.info("POST: challengerID=" + challengerID +" ; CPUCycles=" + cpuMegacycles+" ; source=" + source);                
+            } else {
+            	// Server returned HTTP error code.            	
+            	log.info("POST: Server returned HTTP error code " + responseCode);
+            }
+        } catch (MalformedURLException e) {
+        	log.info("POST: MalformedURLException");
+        } catch (UnsupportedEncodingException e1) {
+        	log.info("POST: UnsupportedEncodingException");
+		} catch (IOException e) {
+        	log.info("POST: IOException");
+		}
+	}
 }
