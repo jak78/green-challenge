49c4cd899966b061b54ca83804e42d0f63771632
julien.jakubowski <julien.jakubowski@75506b02-0ce9-11df-aac2-bbd07866f500>
2010-04-01 21:54:14 +0000

    User can prevent sending samples to the scoring server

--- trunk/GreenFox/srcExtension/defaults/preferences/prefs.js
+++ trunk/GreenFox/srcExtension/defaults/preferences/prefs.js
@@ -1,3 +1,5 @@
 pref("extensions.greenfox.endpoint", "http://score-challenge.appspot.com/api/collect/cpu");
 pref("extensions.greenfox.challengerID", "(email)");
+pref("extensions.greenfox.collection.enabled", true);
+pref("extensions.greenfox.sampling.enabled", true);
 
