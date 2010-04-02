7e9ad7e9a31355b84627b2ae36eacdbe85e48f39
julien.jakubowski <julien.jakubowski@75506b02-0ce9-11df-aac2-bbd07866f500>
2010-04-02 19:14:59 +0000

    version 0.5

--- GreenFox/srcExtension/chrome/GreenFoxExtension/content/greenfox_collect.js
+++ GreenFox/srcExtension/chrome/GreenFoxExtension/content/greenfox_collect.js
@@ -1,32 +1,3 @@
-/*
-GreenFox
-
-Copyright (c) 2010, OCTO Technology
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of GreenFox nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY CONTRIBUTORS ``AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 /**
  * Send samples to the collect server.
  */
@@ -48,6 +19,10 @@ var greenfoxCollect = {
 	},
 	destroy: function() {
 	},
+	willSendSamples: function() {
+		return true
+	},
+
 	/**
 	 * Send sample.
 	 * @param sample sample (number)
@@ -75,10 +50,27 @@ var greenfoxCollect = {
 			// Tutti va bene...
 			return true;
 		} else {
-			// Houston we have a problem....
+			// Don't panic
 			window.alert('GreenFox failed to send sample. Status='+xhr.status+','+xhr.responseText+'\ndata: '+postDataString);
 			return false;
 		}
 	},
 }
 
+var mockCollect = {
+
+	init: function() {},
+	destroy: function() {},
+	willSendSamples: function() {
+		return false
+	},
+
+	/**
+	 * Send sample.
+	 * @param sample sample (number)
+	 */
+	postSample: function(sample) {
+		return true // This is a mock
+	},
+
+}
