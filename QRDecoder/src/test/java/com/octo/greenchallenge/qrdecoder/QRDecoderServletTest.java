d6d1d3b1410d37f1ff601cf396771b4baf55a60c
julien.jakubowski <julien.jakubowski@gmail.com>
2010-05-18 13:50:53 +0000

    fix challengerID parsing pb

--- QRDecoder/src/test/java/com/octo/greenchallenge/qrdecoder/QRDecoderServletTest.java
+++ QRDecoder/src/test/java/com/octo/greenchallenge/qrdecoder/QRDecoderServletTest.java
@@ -2,23 +2,35 @@ package com.octo.greenchallenge.qrdecoder;
 
 import jp.sourceforge.qrcode.QRCodeDecoder;
 import junit.framework.TestCase;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class QRDecoderServletTest extends TestCase {
+    private QRDecoderServlet servlet;
+    private QRCodeDecoder decoder;
 
-	@Test
-	public void testProcessDecode() throws Exception {
+    @Before
+    public void setUp() {
+        servlet = new QRDecoderServlet();
+        decoder = new QRCodeDecoder();
+    }
 
-		QRDecoderServlet servlet = new QRDecoderServlet();
-		QRCodeDecoder decoder = new QRCodeDecoder();
+    @Test
+    public void testProcessDecode() throws Exception {
 
-		String actual = servlet.processDecode("apple.qrc", decoder);
-		String expectedBegin = "BEGIN:VCARD";
-		String expectedEnd = "END:VCARD";
+        String actual = servlet.processDecode("apple.qrc", decoder);
+        String expectedBegin = "BEGIN:VCARD";
+        String expectedEnd = "END:VCARD";
 
-		assertEquals(expectedBegin, actual.substring(0, 11));
-		assertEquals(expectedEnd, actual.substring(actual.length() - 9, actual.length()));
-	}
+        assertEquals(expectedBegin, actual.substring(0, 11));
+        assertEquals(expectedEnd, actual.substring(actual.length() - 9, actual.length()));
+    }
+
+    @Test
+    public void testGetChallengerID() throws Exception {
+        assertEquals("mikaelrob@gmail.com",servlet.getChallengerID("http://qr-decode-mikaelrob.appspot.com"));
+        assertEquals("test-toto@gmail.com",servlet.getChallengerID("http://qr-decode-test-toto.appspot.com"));
+        assertEquals("fake@gmail.com",servlet.getChallengerID("http://localhost:8888/qrdecoder"));
+    }
 
 }
